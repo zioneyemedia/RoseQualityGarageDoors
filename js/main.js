ad6e6173275f7d3659685b4ce47bf2a63f120a57
Dan Woodson <woodson.dan@gmail.com>
2014-01-05 18:53:51 -0600

    Inital paging setup

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+(function($){
+  var showHome = function () {
+        $.get('home.html', function(data){
+          $('.wrapper.main').html(data);
+        })
+      }, 
+      showServices = function () {
+        $.get('services.html', function(data){
+          $('.wrapper.main').html(data);
+        })
+      },
+      showContact = function () {
+        $.get('contact.html', function(data){
+          $('.wrapper.main').html(data);
+        })
+      }, 
+      routes = {
+        '/home': showHome,
+        '/services': showServices,
+        '/contact': showContact
+      },
+      router = Router(routes);
 
+  router.init();
+}(jQuery))
