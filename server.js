661c25b7555ef9ff3ba5fa9e298ef63fb6213ff7
Dan Woodson <woodson.dan@gmail.com>
2014-01-21 18:40:02 +0000

    Added sendgrid sending code

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var connect = require('connect'),
     http = require('http'),
     qs = require('querystring'),
-    port = process.env.PORT || 8080;
+    port = process.env.PORT || 8080,
+    sendgrid = require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
 
 connect()
     .use(connect.logger())
@@ -13,6 +14,16 @@ connect()
           req.on('data', function(chunk) {
             console.log("Received body data:");
             console.log(qs.parse(chunk.toString()));
+            sendgrid.send({
+              to: 'woodson.dan@gmail.com',
+              from: 'site@rosequalitygaragedoors.com',
+              subject: 'Request for contact',
+              text: 'Sending email with NodeJS through SendGrid!'
+              },
+              function(err, json) {
+                if (err) { return console.error(err); }
+                  console.log(json);
+              });
           });
           res.writeHead(200, "OK", {'Content-Type': 'text/html'});
           res.end();
