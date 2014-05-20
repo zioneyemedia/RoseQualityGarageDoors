e84482a27cc96a9e342b45e27becd666dd0aa2b5
zioneyemedia <info@zioneyemedia.com>
2014-05-20 11:37:32 -0500

    restarting from scratch

--- server.js
+++ server.js
@@ -54,37 +54,6 @@ connect()
           next();
         }
       }
-	  if(req.method === 'POST') {
-        if(req.url === '/murfreesboro-garage-door-installation.html'){
-          console.log('Contact Request');
-          req.on('data', function(chunk) {
-            var mesgData = qs.parse(chunk.toString());
-            console.log("Received body data:");
-            console.log(qs.parse(chunk.toString()));
-            sendgrid.send({
-              to: ['info@zioneyemedia.com'],
-              from: 'site@rosequalitygaragedoors.com',
-              subject: 'Contact request from website',
-              text: 'Contact request\n' +
-                    '===============\n' +
-                    'Name: ' + (mesgData.name || '(No name given)')  + '\n' +
-                    'Email: ' + (mesgData.email || '(No email given)')  + '\n'  +
-                    'Phone: ' + (mesgData.phone || '(No phone number given)')  + '\n'  +
-                    'Message: ' + (mesgData.message || '(No additional details)')
-              },
-              function(err, json) {
-                if (err) { return console.error(err); }
-                  console.log(json);
-              });
-          });
-          res.writeHead(200, "OK", {'Content-Type': 'text/html'});
-          res.end();
-        }
-        else{
-          next();
-        }
-      }
-
       else {
         next();
       }
